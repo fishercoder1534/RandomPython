22be5295abaccd9f8b4b26ad30b92b87571e140b
Steve Sun <stevesun@coupang.com>
2016-11-20 10:40:51 -0800

    add a crawler for monitoring Leetcode

---
+++ crawler.py
@@ -0,0 +1,69 @@
+import urllib
+import urllib2
+import requests
+import os
+import json
+import smtplib
+from email.MIMEMultipart import MIMEMultipart
+from email.MIMEText import MIMEText
+
+print "Hello World!"
+
+url = "https://leetcode.com/problemset/algorithms/"
+url2 = "https://leetcode.com/api/problems/algorithms/"
+
+response = urllib2.urlopen(url2)
+data = json.load(response)
+print data
+print data['category_slug']
+# print json.dumps(data, sort_keys=True, indent=4)
+total = data['num_total']
+print 'num_total is: ', total
+
+Leetcode_count_file = "/tmp/leetcode_cnt.txt"
+
+f = open(Leetcode_count_file, 'r')
+previous_count = f.readline()
+previous_count = int(previous_count)
+print "previous_count is: "
+print previous_count
+
+if previous_count != total:
+	print "previous_count != total"
+	os.remove(Leetcode_count_file)
+	print "old file removed"
+	
+	f = open(Leetcode_count_file, 'w')
+	f.write(str(total))
+
+	fromaddr = "sunxx762@umn.edu"
+	toaddr = "steve.j.sun@gmail.com"
+
+	# server = smtplib.SMTP('smtp.gmail.com', 587)
+	server = smtplib.SMTP_SSL('smtp.gmail.com:465')
+	# server.ehlo()#this line is not needed! If uncommented, it'll throw exception!
+	# server.starttls()#this line is not needed! If uncommented, it'll throw exception!
+	# server.login("steve.j.sun@gmail.com", "Betrieber304")
+	server.login(fromaddr, "Ada@2017")#I've enabled less secure apps for this account, so it might now work on other accounts
+
+	msg = MIMEMultipart()
+	msg['From'] = fromaddr
+	msg['To'] = toaddr
+	msg['Subject'] = "Leetcode has new questions!"
+
+	body = "Previous/Now: "
+	body += str(previous_count)
+	body += '/'
+	body += str(total)
+	msg.attach(MIMEText(body, 'plain'))
+	text = msg.as_string()
+	# server.sendmail("steve.j.sun@gmail.com", "sunxx762@umn.edu", text)#this works
+	server.sendmail(fromaddr, toaddr, text)
+	server.quit()
+
+
+
+print "It reached the end!"
+
+
+
