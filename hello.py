a056de1e5a457ac73f9f575c8d85fcdfc9f2e18b
Fisher <fishercoder@gmail.com>
2022-01-29 08:41:43 -0800

    add a simple list assignment example

--- hello.py
+++ hello.py
@@ -9,7 +9,13 @@ print("After encoding with UTF-8, x: ", txt.encode())  # without specifiying enc
 
 print("After encoding with ascii, x: ", txt.encode(encoding="ascii",errors="namereplace"))
 
-for _ in range(10):
-    print("Hello world!")
+# for _ in range(10):
+#     print("Hello world!")
+
+my_list = [1, 2, 3]
+print("Before assignment, my_list: {}".format(my_list))
+
+my_list = ["New Item", 2, 3]
+print("After assignment, my_list: {}".format(my_list))
 
 print("Program finished!")
