0ef1770456745d5e989acb3c5d7d02a89347c7e1
Fisher <fishercoder@gmail.com>
2022-01-29 11:10:56 -0800

    add range() example

--- tuples_sets_booleans.py
+++ tuples_sets_booleans.py
@@ -7,6 +7,18 @@ try:
 except TypeError:
     print("Caught TypeError.")
 
+my_tuple_list = [(1,2),(3,4),(5,6)]
+for t in my_tuple_list:
+    print("t is: ", t)
+
+for (t1, t2) in my_tuple_list:
+    print("t1 is: ", t1)
+    print("t2 is: ", t2)
+
+for t1, t2 in my_tuple_list:
+    print("t2 is: ", t2)
+    print("t1 is: ", t1)
+
 # while lists and dictionaries are mutable and accept assignments
 my_list = [1, 2, 3]
 print(my_list[0])
@@ -25,3 +37,6 @@ print("my_set: ", my_set)
 
 uniq = set([1, 1, 1, 1, 1, 2, 2, 2, 2, 4, 4, 4, 4, 3, 3, 3, 3])
 print("uniq: ", uniq)
+
+for i in range(5):
+    print("i is: ", i)
