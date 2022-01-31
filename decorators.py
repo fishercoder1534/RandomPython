d4f4284db7bef3263d97ff6ecd09163d5e578727
Fisher <fishercoder@gmail.com>
2022-01-30 19:40:57 -0800

    add decorators example

---
+++ decorators.py
@@ -0,0 +1,14 @@
+s = 'GLOBAL VARIABLES'
+
+
+def simple_func():
+    """
+    locals() and globals() are two convenient Python built in function calls.
+    """
+    my_local_var = 10
+    print(locals())
+    print(globals()['s'])
+    print("All global variables are: ", globals())
+
+
+simple_func()
